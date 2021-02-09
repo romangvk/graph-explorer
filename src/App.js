1eeb4630edf1857db1eaca0bff6a184b476afe11
Roman Kasparian <roman.kasparian@gmail.com>
2021-02-09 16:38:55 -0500

    \adding nodes to graph display

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import './App.css';
+import React, { useState } from 'react';
+import GraphDisplay from './components/GraphDisplay';
 
 function App() {
+  const [nodes, setNodes] = useState([{ v: 0, index: 0 }, { v: 1, index: 0 }, { v: 2, index: 0 }]);
+  const [links, setLinks] = useState([{ source: 0, target: 1 }, { source: 1, target: 2 }]);
   return (
     <div className="App">
+      <button onClick={() => {
+        setNodes((oldNodes) => {
+          return [...oldNodes, { v: oldNodes[oldNodes.length - 1].v + 1, index: oldNodes.length }];
+        });
+      }}>Add Nodes</button>
+      <GraphDisplay nodes={nodes} links={links} />
     </div>
   );
 }
