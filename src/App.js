b90c728e29a8a3a94016fea25baaac6cd65f0318
Roman Kasparian <roman.kasparian@gmail.com>
2021-02-15 16:42:07 -0500

    refactored graph to use id instead of index

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import './App.css';
 import React, { useState } from 'react';
 import GraphDisplay from './components/GraphDisplay';
-
+let ID = 5;
 function addNode(g, value) {
-  return { ...g, nodes: [...g.nodes, { v: value, index: g.nodes.length }] };
+  return { ...g, nodes: [...g.nodes, { v: value, id: ID++ }] };
 }
-function removeNode(g, index) {
-  let nodes = g.nodes.filter((node) => node.index !== index);
-  let links = g.links.filter((link) => link.source !== index && link.target !== index);
-  console.log(nodes);
-  console.log(links);
+function removeNode(g, id) {
+  let nodes = g.nodes.filter((node) => node.id !== id);
+  let links = g.links.filter((link) => link.source.id !== id && link.target.id !== id);
   return { ...g, nodes: nodes, links: links };
 }
 
 function App() {
   const [g, setGraph] = useState({
     nodes: [
-      { v: 0, index: 0 },
-      { v: 1, index: 0 },
-      { v: 2, index: 0 }
+      { v: 0, id: 0 },
+      { v: 1, id: 1 },
+      { v: 2, id: 2 }
     ],
     links: [
       { source: 0, target: 1 },
@@ -34,7 +32,7 @@ function App() {
       }}>Add Node</button>
       <button onClick={() => {
         setGraph((old) => {
-          return removeNode(old, 0);
+          return old.nodes.length ? removeNode(old, old.nodes[0].id) : old;
         });
       }}>Remove Node</button>
       <GraphDisplay nodes={g.nodes} links={g.links} />
