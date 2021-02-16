c4096f32be34d93f3c4a33afc44d11d752d66c8a
romangvk <roman.kasparian@gmail.com>
2021-02-16 15:12:10 -0500

    refactored to use graph.js

--- src/App.js
+++ src/App.js
@@ -1,34 +1,24 @@
 import './App.css';
-import React, { useRef, useState } from 'react';
+import React, { useState } from 'react';
 import GraphDisplay from './components/GraphDisplay';
-import graph from './util/graph';
+import * as G from './util/graph';
 
 function App() {
-  const [g, setGraph] = useState({
-    nodes: [
-      { v: 0, id: 0 },
-      { v: 1, id: 1 },
-      { v: 2, id: 2 }
-    ],
-    links: [
-      { source: 0, target: 1 },
-      { source: 1, target: 2 }
-    ]
-  });
-  const G = useRef(new graph());
+  const [graph, setGraph] = useState(G.addNode(G.create(), 0, 1, 2));
   return (
     <div className="App">
       <button onClick={() => {
         setGraph((old) => {
-          return G.addNode(old, Math.floor(Math.random() * 100));
+          let value = Math.floor(Math.random() * 100);
+          return G.addNode(old, value);
         });
       }}>Add Node</button>
       <button onClick={() => {
         setGraph((old) => {
           return old.nodes.length ? G.removeNode(old, old.nodes[0].id) : old;
         });
-      }}>Remove Node</button>z
-      <GraphDisplay nodes={g.nodes} links={g.links} />
+      }}>Remove Node</button>
+      <GraphDisplay nodes={graph.nodes} links={graph.links} />
     </div>
   );
 }
