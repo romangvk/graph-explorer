ee4f97520ffe7cc33dbd355346ebe4b62d82e6c5
romangvk <roman.kasparian@gmail.com>
2021-02-16 21:45:27 -0500

    created floating panel component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import './App.css';
 import React, { useState } from 'react';
 import GraphDisplay from './components/GraphDisplay';
+import FloatingPanel from './components/FloatingPanel';
 import * as G from './util/graph';
 
 function App() {
-  const [graph, setGraph] = useState(G.addNode(G.create(), 0, 1, 2));
+  const [graph, setGraph] = useState(G.addLink(G.addNode(G.create(), 0, 1, 2), 0, 1, 1, 2, 2, 0));
   return (
     <div className="App">
-      <button onClick={() => {
-        setGraph((old) => {
-          let value = Math.floor(Math.random() * 100);
-          return G.addNode(old, value);
-        });
-      }}>Add Node</button>
-      <button onClick={() => {
-        setGraph((old) => {
-          return old.nodes.length ? G.removeNode(old, old.nodes[0].id) : old;
-        });
-      }}>Remove Node</button>
+      <FloatingPanel>
+        <button onClick={() => {
+          setGraph((old) => {
+            let value = Math.floor(Math.random() * 100);
+            return G.addNode(old, value);
+          });
+        }}>Add Node</button>
+        <button onClick={() => {
+          setGraph((old) => {
+            return old.nodes.length ? G.removeNode(old, old.nodes[0].id) : old;
+          });
+        }}>Remove Node</button>
+        <button onClick={() => {
+          setGraph((old) => {
+            return G.addLink(old, 0, 1);
+          });
+        }}>Add Link</button>
+      </FloatingPanel>
       <GraphDisplay nodes={graph.nodes} links={graph.links} />
     </div>
   );
