7845af05c74d8c27baab7fde69fe7c5883317b94
romangvk <roman.kasparian@gmail.com>
2021-02-27 00:49:30 -0500

    added icon button and generalized functionality

--- src/components/NodeEditor.js
+++ src/components/NodeEditor.js
@@ -1,11 +1,23 @@
-// import './NodeEditor.css';
-import React, { useRef } from 'react';
+import './NodeEditor.css';
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-function NodeEditor({ edit, remove, value, inputRef }) {
+function NodeEditor({ change, action, enterAction, value, icon, inputRef }) {
+    const enter = (event) => {
+        if (event.key === 'Enter') {
+            enterAction ? enterAction() : action();
+        }
+    }
     return (
         <div className="node">
-            <input ref={inputRef} type="text" value={value} onChange={(e) => edit(e.target.value)}></input>
-            <button onClick={() => remove()}>x</button>
+            <input
+                placeholder="value"
+                type="text"
+                ref={inputRef}
+                value={value}
+                onKeyDown={enter}
+                onChange={(e) => change(e.target.value)} />
+            <div className="action"><FontAwesomeIcon icon={icon} size="xs" fixedWidth onClick={() => action()} /></div>
         </div>
     );
 }
