2c385910ee8f08258180287e85cc306047c3ed04
romangvk <roman.kasparian@gmail.com>
2021-02-27 17:03:47 -0500

    passing top/left positioning values to child

--- src/components/FloatingPanel.js
+++ src/components/FloatingPanel.js
@@ -4,11 +4,11 @@ import Draggable from 'react-draggable';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlus, faMinus } from '@fortawesome/free-solid-svg-icons';
 
-function FloatingPanel({ title, children }) {
+function FloatingPanel({ title, children, top, left }) {
     const [expand, toggleExpand] = useState(true);
     return (
         <Draggable>
-            <div className={`floating-panel ${expand ? "floating-panel-max" : "floating-panel-min"}`}>
+            <div className={`floating-panel ${expand ? "floating-panel-max" : "floating-panel-min"}`} style={{ top, left }}>
                 <div className="floating-panel-title">
                     <b>{title}</b>
                     <div className="action">
