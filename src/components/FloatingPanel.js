d6b3a6b935872d8aabb4424e229fa582bb544435
romangvk <roman.kasparian@gmail.com>
2021-02-27 05:49:21 -0500

    added expand and minimize button

--- src/components/FloatingPanel.js
+++ src/components/FloatingPanel.js
@@ -1,15 +1,25 @@
 import './FloatingPanel.css';
-import React from 'react';
+import React, { useState } from 'react';
 import Draggable from 'react-draggable';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus, faMinus } from '@fortawesome/free-solid-svg-icons';
 
 function FloatingPanel({ title, children }) {
+    const [expand, toggleExpand] = useState(true);
     return (
         <Draggable>
-            <div className="floating-panel">
-                <div className="floating-panel-title"><b>{title}</b></div>
-                <div className="floating-panel-content">
-                    {children}
+            <div className={`floating-panel ${expand ? "floating-panel-max" : "floating-panel-min"}`}>
+                <div className="floating-panel-title">
+                    <b>{title}</b>
+                    <div className="action">
+                        <FontAwesomeIcon icon={expand ? faMinus : faPlus} fixedWidth onClick={() => {
+                            toggleExpand(!expand);
+                        }} />
+                    </div>
                 </div>
+                {expand ? <div className="floating-panel-content">
+                    {children}
+                </div> : null}
             </div>
         </Draggable>
     );
