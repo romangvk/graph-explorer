b73456119b57f84c7e3a637e662a5472203c24c1
romangvk <roman.kasparian@gmail.com>
2021-02-27 00:55:39 -0500

    organized control panel elements

--- src/App.js
+++ src/App.js
@@ -1,44 +1,72 @@
 import './styles.css';
 import React, { useRef, useState } from 'react';
 import GraphDisplay from './components/GraphDisplay';
-import SidePanel from './components/SidePanel';
 import NodeEditor from './components/NodeEditor';
+import LinkEditor from './components/LinkEditor';
 import * as G from './util/graph';
+import { faPlus, faTimes } from '@fortawesome/free-solid-svg-icons'
 
 function App() {
   const [graph, setGraph] = useState(G.addLink(G.addNode(G.create(), 0, 1, 2), 1, 2, 2, 0));
   const nodeRefs = useRef({})
-
+  const addNodeRef = useRef();
+  const [addV, setAddV] = useState("");
 
   return (
     <div className="App">
-      <SidePanel>
-        <button onClick={() => {
-          setGraph((old) => {
-            let value = Math.floor(Math.random() * 100);
-            return G.addNode(old, value);
-          });
-        }}>Add Node</button>
-        <div className="nodes">
-          {graph.nodes.map((node, i) => {
-            return (
-              <NodeEditor value={node.v} inputRef={(el) => (nodeRefs.current[node.id] = el)}
-                edit={(v) => {
-                  setGraph((old) => G.updateNode(old, node.id, v));
-                }}
-                remove={() => {
-                  setGraph((old) => G.removeNode(old, node.id));
-                }} />
-            );
-          })}
+      <div className="side-panel">
+        <div className="half-panel">
+          <div className="half-panel-title"><b>Nodes</b></div>
+          <div className="half-panel-content">
+            <NodeEditor value={addV} inputRef={addNodeRef}
+              change={(v) => setAddV(v)}
+              action={() => {
+                if (G.contains(graph, addV)) {
+                  console.log("Must be a new value");
+                } else {
+                  setGraph((old) => G.addNode(old, addV));
+                  setAddV("");
+                }
+                addNodeRef.current.focus();
+              }}
+              icon={faPlus} />
+            <div className="list">
+              {graph.nodes.map((node, i) => {
+                return (
+                  <NodeEditor key={i} value={node.v} inputRef={(el) => (nodeRefs.current[node.id] = el)}
+                    change={(v) => {
+                      G.contains(graph, v) ?
+                        setGraph((old) => { return { ...old } })
+                        :
+                        setGraph((old) => G.updateNode(old, node.id, v))
+                    }}
+                    action={() => setGraph((old) => G.removeNode(old, node.id))}
+                    icon={faTimes} />
+                );
+              })}
+            </div>
+          </div>
         </div>
-      </SidePanel>
+        <div className="half-panel">
+          <div className="half-panel-title"><b>Links</b></div>
+          <div className="half-panel-content">
+            <div className="list">
+              {graph.links.map((link, i) => {
+                return (
+                  <LinkEditor key={i} source={link.source.v} target={link.target.v}
+                    icon={faTimes} />
+                );
+              })}
+            </div>
+          </div>
+        </div>
+      </div>
       <GraphDisplay nodes={graph.nodes} links={graph.links}
-      onClickNode={(d) => {
-        nodeRefs.current[d.id].focus();
-        nodeRefs.current[d.id].select();
-        nodeRefs.current[d.id].scrollIntoView();
-      }}
+        onClickNode={(d) => {
+          nodeRefs.current[d.id].focus();
+          nodeRefs.current[d.id].select();
+          nodeRefs.current[d.id].scrollIntoView();
+        }}
       />
     </div >
   );
