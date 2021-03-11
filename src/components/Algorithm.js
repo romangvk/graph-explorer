eed23f4cf877cc4d399deb8767c6b4db4b565324
Roman Kasparian <roman.kasparian@gmail.com>
2021-03-10 22:25:52 -0500

    passing input values through to action

--- src/components/Algorithm.js
+++ src/components/Algorithm.js
@@ -1,10 +1,10 @@
 import './Algorithm.css';
-import React, { useRef } from 'react';
+import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
 
 function Algorithm({ name, action, args, nodes }) {
-    const inputRefs = useRef({});
+    const [argValues, setArgs] = useState(args.map(() => null));
     return (
         <code className="algorithm">
             {name}&nbsp;
@@ -13,13 +13,19 @@ function Algorithm({ name, action, args, nodes }) {
                     <select
                         defaultValue={arg}
                         key={i}
-                        ref={(el) => (inputRefs.current[arg] = el)}>
+                        onChange={(e) => {
+                            setArgs((oldValues) => {
+                                let newValues = [...oldValues];
+                                newValues[i] = e.target.value;
+                                return newValues;
+                            });
+                        }}>
                         <option disabled hidden>{arg}</option>
                         {nodes && nodes.map((node, j) => <option key={j} value={node.id}>{node.v}</option>)}
                     </select>
                 );
             })}
-            <div className="action"><FontAwesomeIcon icon={faArrowRight} fixedWidth onClick={() => action && action()} /></div>
+            <div className="action"><FontAwesomeIcon icon={faArrowRight} fixedWidth onClick={() => action && action(...argValues.map((value) => parseInt(value, 10)))}/></div>
         </code>
     );
 }
